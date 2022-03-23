c3399c76ff2528791d6b63b1b7a43aea42c86c44
anandhanjg <jganandhanddevlabs@gmail.com>
2022-03-23 10:48:58 +0530

    audio-player added

--- src/app/components/audio/audio.component.ts
+++ src/app/components/audio/audio.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
 import { FileService } from 'src/app/services/file-service/file.service';
 
 @Component({
@@ -6,14 +7,81 @@ import { FileService } from 'src/app/services/file-service/file.service';
   templateUrl: './audio.component.html',
   styleUrls: ['./audio.component.scss']
 })
-export class AudioComponent implements OnInit {
+export class AudioComponent implements OnInit, OnDestroy {
   audios:any[]=[];
-  constructor(private fs:FileService) { }
-  audio:any=document.createElement('audio');
+  profile:any;
+  volume:number=20;
+  audio:any=document.createElement('audio');;
+  constructor(private fs:FileService,private as:AuthService) { 
+    
+  }
+  ngOnDestroy(): void {
+    // throw new Error('Method not implemented.');
+    console.log("DESTROYING PAGE");
+    this.audio.pause();
+    this.audio.src=null;
+    // this.audio=null;
+  }
+  
+  songPlaying:boolean=false;
+  songIndex:number=0;
   ngOnInit(): void {
+    console.log("ON INIT");
+    // this.audio=document.createElement('audio');
+    this.profile=this.as.returnProfile();
+    this.setVolume();
     this.fs.audios.subscribe(v=>{
       this.audios=this.fs.audios.value.filter(a=>this.audio.canPlayType(a.mimeType));
     })
+    this.audio.onended=this.playNext.bind(this);
+  }
+
+  playOrPause(){
+    if(!this.songPlaying){
+      this.playSong(true);
+    }else{
+      this.pauseSong();
+    }
+    // this.songPlaying=!this.songPlaying;
+  }
+
+  playSong(playCurrent?:boolean){
+    if((playCurrent && !this.audio.src) || !playCurrent)
+      this.audio.src='https://anandhan.ddl.link:4000/'+this.profile.username+'/'+this.audios[this.songIndex].path;
+    this.audio.play();
+    this.songPlaying=true;
+  }
+
+  pauseSong(){
+    this.audio.pause();
+    this.songPlaying=false;
+  }
+
+  playNext(){
+    if(this.songIndex==this.audios.length-1){
+      this.songIndex=0;
+    }else{
+      this.songIndex+=1;
+    }
+    this.playSong();
+  }
+
+  playPrevious(){
+    if(this.songIndex>0){
+      this.songIndex-=1;
+    }else{
+      this.songIndex=this.audios.length-1;
+    }
+    this.playSong();
+  }
+
+  changeVolume(event:any){
+    this.volume=event.target.value;
+    this.setVolume()
+  }
+
+  setVolume(){
+    this.audio.volume=this.volume/100;
   }
 
 }
